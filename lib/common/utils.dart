841d5e78e01b46349ae924a356281c364e0ee181
Aulia Putri <153080892+ulzptrzz@users.noreply.github.com>
2024-11-02 20:34:03 +0700

    two commit

---
+++ lib/common/utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const apiKey = "";
+const kBackgroundColor = Colors.black;
+const imageUrl = "";
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movieapps/common/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,8 +11,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        title: Image.asset(
+          "assets/logo.png",
+          height: 50,
+          width: 120,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.search,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              height: 27,
+              width: 27,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          )
+        ],
+      ),
+      body: const Center(
         child: Text("Hi Im homescreen"),
       ),
     );
