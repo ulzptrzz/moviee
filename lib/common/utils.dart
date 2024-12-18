3c1d1feebf5fda91c797b495b0da61db2b542cb6
Akmxyyxd683 <152455534+Akmxyyxd683@users.noreply.github.com>
2024-12-18 21:26:29 +0700

    p

--- lib/common/utils.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const apiKey = "827b24bfa6c2d3d740d63c4fdf6c9952";
-const kBackgroundColor = Colors.black;
-const imageUrl = "https://image.tmdb.org/t/p/w500";
